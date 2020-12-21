e97b014b20bb16d57cae2d95a7da3325849557fb
Simon Bruklich <bruklich.s@northeastern.edu>
2020-12-21 00:36:36 +0000

    Adjust padding-top for Disclaimer

--- src/Components/Disclaimer.jsx
+++ src/Components/Disclaimer.jsx
@@ -1,28 +1,16 @@
-import React, { useRef, useLayoutEffect, useState } from "react";
+import React from "react";
 import GeneralDisclaimer from "./Disclaimers/General";
 import medicalHealthDisclaimer from "./Disclaimers/Medical";
 
 const Disclaimer = () => {
-  const targetRef = useRef();
-  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-
-  useLayoutEffect(() => {
-    if (targetRef.current) {
-      setDimensions({
-        width: targetRef.current.offsetWidth,
-        height: targetRef.current.offsetHeight,
-      });
-    }
-  }, []);
-
-  console.log("simon height: ", dimensions.height);
-
   return (
-    <div ref={targetRef} className="disclaimer">
-      <h1 className="disclaimer-header">Disclaimer</h1>
-      <div className="disclaimer-text">
-        <p>{GeneralDisclaimer}</p>
-        <p>{medicalHealthDisclaimer}</p>
+    <div className="disclaimer-wrap">
+      <div className="disclaimer">
+        <h1 className="disclaimer-header">Disclaimer</h1>
+        <div className="disclaimer-text">
+          <p>{GeneralDisclaimer}</p>
+          <p>{medicalHealthDisclaimer}</p>
+        </div>
       </div>
     </div>
   );
