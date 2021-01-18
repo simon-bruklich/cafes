6a6cbfa855b25f4a89f7505a8b7114aea423e1cb
Simon Bruklich <bruklich.s@northeastern.edu>
2021-01-18 18:13:18 +0000

    Updates

---
+++ precache-manifest.df973a0dc984e884772d701e3026ad5e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb8c11210d7f325f275205feb117a46",
+    "url": "/Cafes/index.html"
+  },
+  {
+    "revision": "66dddde3746d457a061a",
+    "url": "/Cafes/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "3b9c1b36dd599b26d7db",
+    "url": "/Cafes/static/css/main.a9b2a51f.chunk.css"
+  },
+  {
+    "revision": "66dddde3746d457a061a",
+    "url": "/Cafes/static/js/2.6df79712.chunk.js"
+  },
+  {
+    "revision": "98cab8a5b693ca0381ebe7864fc605d2",
+    "url": "/Cafes/static/js/2.6df79712.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9c1b36dd599b26d7db",
+    "url": "/Cafes/static/js/main.73cd15c2.chunk.js"
+  },
+  {
+    "revision": "5cff230bbe3e86c22ddb",
+    "url": "/Cafes/static/js/runtime-main.73b46874.js"
+  },
+  {
+    "revision": "1018aaef964fdf9038cbb174e0eaf4c0",
+    "url": "/Cafes/static/media/Cafes.1018aaef.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Cafes-Covid-School-Tracker/precache-manifest.ee6fea68fc1d0db4f8bf8b8c137597a0.js"
+  "/Cafes/precache-manifest.df973a0dc984e884772d701e3026ad5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cafes-Covid-School-Tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Cafes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
